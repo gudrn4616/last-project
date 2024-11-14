e33aa225cdc6ed6cf7a72726a2bf45c91d6268ec
조정현 <emforhs0913@naver.com>
2024-11-14 09:46:42 +0900

    remove: GameRoomManager 코드 삭제
    및 gitignore코드 일부 수정

--- Server/BattleServer/src/contents/room/GameRoomManager.js
+++ Server/BattleServer/src/contents/room/GameRoomManager.js
@@ -13,15 +13,7 @@ class GameRoomManager {
    * @param {GamePlayer} player - 입장할 플레이어 정보
    ---------------------------------------------*/
   enterRoomHandler(roomId, player) {
-    console.log('enterRoomHandler');
 
-    const room = this.rooms.get(roomId);
-    if (!room) {
-      console.log('유효하지 않은 roomId입니다.');
-      return;
-    }
-
-    room.enterRoom(player);
   }
 
   /**---------------------------------------------
@@ -30,44 +22,7 @@ class GameRoomManager {
    * @param {LobbySession | BattleSession} session - 요청한 세션
    ---------------------------------------------*/
   createGameRoomHandler(buffer, session) {
-    console.log('createGameRoom', session.getId());
-    console.log('--------------------');
-    const L2B_CreateRoomPacket = fromBinary(L2B_CreateRoomSchema, buffer);
-    const roomId = L2B_CreateRoomPacket.roomId;
-    const maxPlayerCount = L2B_CreateRoomPacket.maxPlayers;
 
-    // roomId가 이미 존재하는지 검증
-    if (this.rooms.has(roomId)) {
-      console.error(`방 ID ${roomId}가 이미 존재합니다.`);
-      console.log(this.rooms.get(roomId));
-      throw new CustomError(ErrorCodes.SOCKET_ERROR, '방 ID가 이미 존재합니다.');
-    }
-
-    // 최대 인원 수가 유효한지 검증
-    if (maxPlayerCount <= 0 || maxPlayerCount > MAX_ROOMS_SIZE) {
-      console.error(`유효하지 않은 최대 인원 수: ${maxPlayerCount}`);
-      throw new CustomError(ErrorCodes.SOCKET_ERROR, '유효하지 않은 최대 인원 수입니다.');
-    }
-
-    // 게임 방 생성
-    const newGameRoom = new GameRoom(roomId, maxPlayerCount);
-    this.rooms.set(roomId, newGameRoom);
-
-    console.log('방 생성', L2B_CreateRoomPacket.roomId);
-
-    const B2L_CreateRoomPacket = create(B2L_CreateRoomSchema, {
-      isCreated: true,
-      roomId: L2B_CreateRoomPacket.roomId,
-    });
-
-    const sendBuffer = PacketUtils.SerializePacket(
-      B2L_CreateRoomPacket,
-      B2L_CreateRoomSchema,
-      ePacketId.B2L_CreateRoom,
-      session.getNextSequence()
-    );
-    session.send(sendBuffer);
-    console.log('send B2L_CreateRoom');
   }
 
   /**---------------------------------------------
@@ -76,15 +31,7 @@ class GameRoomManager {
    * @param {BattleSession} session - 이동 요청을 보낸 세션
    ---------------------------------------------*/
   moveHandler(buffer, session) {
-    const packet = fromBinary(C2B_MoveSchema, buffer);
 
-    const room = this.rooms.get(packet.roomId);
-    if (!room) {
-      console.log('유효하지 않은 roomId');
-      throw new CustomError(ErrorCodes.SOCKET_ERROR, '유효하지 않은 roomId');
-    }
-
-    room.handleMove(buffer);
   }
 }
 
